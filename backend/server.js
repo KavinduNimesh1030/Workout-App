75b314530b42576a49836f9a088042225a1e191d
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-06-19 11:40:36 +0530

    connected to mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const workoutRoutes = require('./routes/workout')
-
+const mongoose = require('mongoose');
 //express app
 const app = express()
 
@@ -10,7 +10,15 @@ app.use(express.json())
 //routes
 app.use('/api/workouts',workoutRoutes)
 
+mongoose.connect(
+    "mongodb://127.0.0.1:27017/workout"
+).then(()=>{
 //listner
 app.listen(4000,()=>{
-    console.log("App runing on port 4000")
+    console.log("Connected to db & App runing on port 4000")
 })
+
+}).catch((err)=>{
+    console.error(err)
+})
+
