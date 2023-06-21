f56fd5be7c566b32c04ee8e70ad1b82c8db64cb6
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-06-22 00:03:22 +0530

    delete workout function created

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
 const workoutdetails = ({workout}) => {
+
+  const deleteWorkout = async()=>{
+    const response =  await fetch('/api/workouts/'+workout._id,{
+      method : 'DELETE',
+    })
+
+    const json =  await response.json()
+
+    if(!response.ok){
+        alert(json.error)
+    }else{
+        alert('Deleted..!',json)
+    }
+    
+
+  }
   return (
     <div className='workout-details'>
       <h4>{workout.title}</h4>
       <p><storng>load : </storng>{workout.load}</p>
       <p><storng>Reps : </storng>{workout.reps}</p>
       <p><storng>Created at : </storng>{workout.createdAt}</p>
+      <button onClick={deleteWorkout}>delete</button>
     </div>
   )
 }
